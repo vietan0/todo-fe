afdf312e3165005cc330770cf7a7dd3a6d565235
Viet An <tiktaktictac269@gmail.com>
2024-07-02 11:30:25 +0700

    fix: match overlay's width to subtask's width

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -89,11 +89,11 @@ const Task = forwardRef<HTMLAnchorElement, Props>(({
       {...listeners}
       {...props}
       className={cn(
-        task.completed ? 'opacity-disabled' : 'hover:bg-default-100 hover:opacity-hover focus:bg-default-100 focus-visible:z-10 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-focus',
-        'h-auto min-h-[55px] w-full items-start justify-start border border-default bg-default-50 p-3 text-start text-sm',
-        'group relative z-0 box-border inline-flex min-w-20 select-none appearance-none gap-2 whitespace-nowrap rounded-small font-normal text-foreground no-underline subpixel-antialiased outline-none transition-opacity tap-highlight-transparent active:opacity-disabled [&>svg]:max-w-[theme(spacing.8)]',
-        isDragging && `before:absolute before:-left-2.5 before:-top-3 before:ml-0.5 before:size-3 before:rounded-full before:border-3 before:border-primary`,
-        isDragging && `after:absolute after:-top-2 after:left-0 after:ml-0.5 after:h-0.5 after:w-full after:bg-primary`,
+        task.completed ? 'opacity-disabled' : 'hover:bg-default-100 hover:opacity-hover focus-visible:z-10 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-focus',
+        'h-auto min-h-[55px] items-start justify-start border border-default bg-default-50 p-3 text-start text-sm',
+        'group relative z-0 box-border inline-flex min-w-20 select-none appearance-none gap-2 whitespace-nowrap rounded-small font-normal text-foreground no-underline subpixel-antialiased transition-opacity tap-highlight-transparent active:opacity-disabled [&>svg]:max-w-[theme(spacing.8)]',
+        isDragging && 'before:absolute before:-left-2.5 before:-top-3 before:ml-0.5 before:size-3 before:rounded-full before:border-3 before:border-primary',
+        isDragging && 'after:absolute after:-top-2 after:left-0 after:ml-0.5 after:h-0.5 after:w-full after:bg-primary',
         isOverlay && 'z-50 cursor-grabbing border border-primary',
       )}
       onBlur={isOverlay ? undefined : () => setIsHover(false)}
@@ -101,7 +101,11 @@ const Task = forwardRef<HTMLAnchorElement, Props>(({
       onFocus={isOverlay ? undefined : () => setIsHover(true)}
       onMouseEnter={isOverlay ? undefined : () => setIsHover(true)}
       onMouseLeave={isOverlay ? undefined : () => setIsHover(false)}
-      style={{ ...style, marginLeft: finalIndent }}
+      style={{
+        ...style,
+        marginLeft: isOverlay ? 0 : finalIndent,
+        width: isOverlay ? '100%' : `calc(100% - ${finalIndent}px)`,
+      }}
     >
       <Checkbox
         classNames={{
