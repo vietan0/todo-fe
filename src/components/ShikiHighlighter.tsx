ee88c6489971163fd0703ecca4b2549535bad28f
Viet An <tiktaktictac269@gmail.com>
2025-09-16 19:54:58 +0700

    fix: stop code block from flashing "Rendering codeblock..."

--- src/components/ShikiHighlighter.tsx
+++ src/components/ShikiHighlighter.tsx
@@ -2,19 +2,25 @@ import parse from 'html-react-parser';
 import { useEffect, useState } from 'react';
 import { bundledLanguages, codeToHtml } from 'shiki';
 
+const codeCache = new Map();
+
 export default function ShikiHighlighter({ code, lang }: { code: string; lang?: string }) {
-  const [html, setHtml] = useState('<p class="text-sm">Rendering codeblock...</p>');
+  const cacheKey = `${code}-${lang}`;
+  const [html, setHtml] = useState(codeCache.has(cacheKey) ? codeCache.get(cacheKey) : '<p class="text-sm">Rendering codeblock...</p>');
   const shikiApprovedLang = typeof lang === 'string' && lang in bundledLanguages ? lang : 'text';
 
   useEffect(() => {
-    codeToHtml(code, {
-      lang: shikiApprovedLang,
-      theme: 'dark-plus',
-    }).then((val) => {
-      setHtml(val);
-    }).catch((err) => {
-      console.error(err);
-    });
+    if (!codeCache.has(cacheKey)) {
+      codeToHtml(code, {
+        lang: shikiApprovedLang,
+        theme: 'dark-plus',
+      }).then((val) => {
+        setHtml(val);
+        codeCache.set(cacheKey, val);
+      }).catch((err) => {
+        console.error(err);
+      });
+    }
   }, [code, lang]);
 
   const reactElem = parse(html);
